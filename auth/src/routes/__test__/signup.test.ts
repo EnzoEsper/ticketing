7460bdcfb3f0bbafcfc7e91c89c43c72321a95d8
enzoesper <enzoesper@hotmail.com>
2020-09-17 15:15:34 -0300

    testing that after a successful signup the response has a header of 'Set-Cookie'

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -63,4 +63,16 @@ it('not allows duplicate emails', async () => {
       password: 'password'
     })
     .expect(400);
+});
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+  
+  expect(response.get('Set-Cookie')).toBeDefined();
 })
