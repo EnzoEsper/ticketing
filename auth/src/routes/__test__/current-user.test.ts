def03860a7bf86f1eeb8519d35795abdf60e699a
enzoesper <enzoesper@hotmail.com>
2020-09-17 21:04:48 -0300

    testing the case for non authenticated user for the current user route handler

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -14,3 +14,12 @@ it('response with details about the current user', async () => {
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 })
+
+it('response with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200)
+
+  expect(response.body.currentUser).toEqual(null);
+})
