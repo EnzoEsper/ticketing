a97b2e4cfd19585ff81588e9beeed7dd3dc56a66
enzoesper <enzoesper@hotmail.com>
2020-09-20 16:11:27 -0300

    passing through the cookie passing directly the headers received in the req object that is passed as a parameter to getInitialProps, so it acts as a sort of proxy

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,17 +9,15 @@ const LandingPage = ({ currentUser }) => {
 // this function is where we are going to attempt to fetch some data specifically for doing some initial rendering of the app. 
 // Any data that is returned from this function is gonna show up inside the component.
 // If we need to fetch some data during the ssr process, WE HAVE TO DO IT INSIDE getInitialProps and we can not do it inside of the component. 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   // we can not use the useRequest custom hook inside here, bc hooks are used iside of react components and this is a plain function (and require to inmediately return some data)
   
   if (typeof window === 'undefined') {
     // we are on the server -> requests should be made to 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
     const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-      headers: {
-        Host: 'ticketing.dev'
-      }
+      headers: req.headers
     });
-    
+
     return data;
   } else {
     // we are on the browser -> request can be made with a base url of '' and rely upon the browser to put on the base domain for us
