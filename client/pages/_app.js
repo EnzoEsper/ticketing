a2086adf814640d315df082c77c5b230159b2e26
enzoesper <enzoesper@hotmail.com>
2020-09-20 16:53:13 -0300

    Adding the header to the _app.js as a wrapper for the components so it is showed in all of them

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,5 +5,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 // whenever we try to visit some component, next is going to import that component and passing it into this custom component as the Component prop, 
 // and then pagePropos are gonna be the set of components that we possibly pass to the Component   
 export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return(
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
