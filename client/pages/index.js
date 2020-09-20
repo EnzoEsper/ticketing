0d579ea61c3de93571a59be2ac17eed99377a4e0
enzoesper <enzoesper@hotmail.com>
2020-09-20 12:21:41 -0300

    getInitialProps: little overview of server side rendering process with next

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
-  return <h1>Landing Page</h1>
+const LandingPage = ({ color }) => {
+  console.log(`I am in the comoponent ${color}`);
+  return <h1>Landing Page</h1>;
 };
+
+// This is gonna be executed during the server side rendering process, this function is where we are going to attempt to fetch some data 
+// specifically for doing some initial rendering of the app. Any data that is returned from this function is gonna show up inside the component.
+// If we need to fetch some data during the ssr process, WE HAVE TO DO IT INSIDE getInitialProps and we can not do it inside of the component. 
+LandingPage.getInitialProps = () => {
+  console.log(`I am on the server!`);
+
+  return { color: 'red' };
+};
+
+export default LandingPage;
