8c9bd9d29598c16b5168aee85962eb55abb344e0
enzoesper <enzoesper@hotmail.com>
2020-09-20 16:29:48 -0300

    Modifying the content on the landing page depending whether the user is signed in or not

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,7 @@
 import buildClient from '../api/buildClient';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing Page</h1>;
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 };
 
 // This is gonna be executed during the server side rendering process (can be executed in the browser under very particular circumstances), 
