2473179b1561c21b48d17459a0b4cd0289b80dd0
enzoesper <enzoesper@hotmail.com>
2020-09-23 13:47:05 -0300

    defining the mongo uri for the tickets service inside an environment variable in the tickets-depl.yaml config file

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,9 +7,13 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+  // check to make sure that the mongo uri string is defined
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
