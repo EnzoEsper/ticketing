ac995857df26274da5ee5533016d4f32fea8373e
enzoesper <enzoesper@hotmail.com>
2020-09-23 13:34:59 -0300

    setting up initial files and configuration files for the tickets service

---
+++ tickets/src/index.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { app } from './app';
+
+const start = async () => {
+
+  // adding a check to make sure that the environment variable is actually defined
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('Connected to mongoDb');
+  } catch (err) {
+    console.error(err)
+  }
+
+  app.listen(3000, () => {
+    console.log("Ticket service listening on port 3000!");
+  });
+}
+
+start();
+
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,38 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+// before all the differents tests start up a new instance of the mongodb memory server is created, this starts a new copy of mongodb
+// in-memory that allows run multiple different test suits at the same time across different projects without them all trying to reach out to the same copy of mongo 
+let mongo: any;
+beforeAll(async () => {
+  // setting up the environment variable wit the jwt secret
+  // because the environment variable defined in the signup.ts file only gets defined when we run our code inside a pod in the k8s cluster 
+  process.env.JWT_KEY = 'asdf';
+
+  // starting up mongo memory server and connectign mongoose to it
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+});
+
+// before each test starts we are going to reach inside the mongodb database and delete all the data inside there
+beforeEach(async () => {
+  // obtaining all the different collections that exists
+  const collections = await mongoose.connection.db.collections();
+
+  // deleting all the data inside all collections
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// after we finish running all our different tests we stop the mongo db memory server and we disconnect mongoose from it as well 
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+})
