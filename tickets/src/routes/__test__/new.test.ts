f28131d929ce53d1972e8eabf3f5e35d3b7d0f01
enzoesper <enzoesper@hotmail.com>
2020-09-30 19:40:02 -0300

    validating title and price for a ticket

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -28,11 +28,41 @@ it('returns a status other than 401 if the user is signed in', async () => {
 })
 
 it('returns an error if an invalid title is provided', async () => {
-
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signup())
+    .send({
+      title: '',
+      price: 10
+    })
+    .expect(400)
+  
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signup())
+    .send({
+      price: 10
+    })
+    .expect(400)
 });
 
 it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signup())
+    .send({
+      title: 'eassfsadsafasf',
+      price: -10
+    })
+    .expect(400)
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signup())
+    .send({
+      title: 'eassfsadsafasf',
+    })
+    .expect(400)
 });
 
 it('creates a ticket with valid inputs', async () => {
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@tickgit/common';
+import { requireAuth, validateRequest } from '@tickgit/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth,  async (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
